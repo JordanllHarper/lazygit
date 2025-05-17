c1865ff5d3f0cf22ec7898cb762c11e385491d07
Jordan <46637683+JordanllHarper@users.noreply.github.com>
2025-05-17 17:55:04 +0100

    Add name to SetRemoteRepository

--- pkg/commands/git_commands/clone.go
+++ pkg/commands/git_commands/clone.go
@@ -8,8 +8,8 @@ func NewCloneCommands(gitCommon *GitCommon) *CloneCommands {
 	return &CloneCommands{GitCommon: gitCommon}
 }
 
-func (self *CloneCommands) SetRemoteRepository(url string) error {
-	cmdArgs := NewGitCmd("clone").Arg(url).ToArgv()
+func (self *CloneCommands) SetRemoteRepository(url string, name string) error {
+	cmdArgs := NewGitCmd("clone").Arg(url).Arg(name).ToArgv()
 
 	return self.cmd.New(cmdArgs).Run()
 }
